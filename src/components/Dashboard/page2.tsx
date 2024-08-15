b9f6e36b2e51dad47c37c71e112eba315c398122
temidayoakinyemi <temidayoakinyemi886@gmail.com>
2024-08-15 17:29:37 -0600

    URL Shortener

--- src/components/Dashboard/page2.tsx
+++ src/components/Dashboard/page2.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,  } from "react";
+import React, { useState, useEffect } from "react";
 import { QRCodeCanvas } from "qrcode.react";
 import { shortenUrl } from "../../Api/Shorten";
 import {
